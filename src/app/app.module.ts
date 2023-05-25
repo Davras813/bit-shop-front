a724d3b5cc1124f22baa8fb4a63a81a53d44c34a
David <davidloaiza13@gmail.com>
2023-05-24 20:39:55 -0500

    carga ejercicio en clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HomeComponent } from './routes/home/home.component';
+import { LoginComponent } from './routes/login/login.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ButtonComponent,
+    HomeComponent,
+    LoginComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
